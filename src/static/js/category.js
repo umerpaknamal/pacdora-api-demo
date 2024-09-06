5a905146681f929c31de942fdf31dff20a138c25
yanghang <yanghang@baoxiaohe.com>
2024-09-06 18:47:53 +0800

    optimize: add simple data

--- src/static/js/category.js
+++ src/static/js/category.js
@@ -14,7 +14,52 @@
   const result = await fetch(
     `/models?current=1&pageSize=50&mockupNameKey=${mockupKey}`
   ).then((res) => res.json());
-  const list = result.data;
+  const backupData = {
+    Boxes: [
+      {
+        image: "https://cdn.pacdora.com/render/20240115/102709970001.jpg",
+        showName: "Flip top boxes mailer all-in-one mockup 150010",
+        modelId: 150010,
+      },
+      {
+        image: "https://cdn.pacdora.com/render/20240522/104332490001.jpg",
+        showName:
+          "Tuck end boxes double tray reverse mortise lock mockup 100010",
+        modelId: 100010,
+      },
+    ],
+    Bottles: [
+      {
+        image: "https://cdn.pacdora.com/render/20240104/102624380001.jpg",
+        showName: "Cans aluminum bottles tin mockup 500010",
+        modelId: 500010,
+      },
+      {
+        image: "https://cloud.pacdora.com/render/20230625/101102260001.jpg",
+        showName: "Plastic bottles product label mockup 500340",
+        modelId: 500340,
+      },
+    ],
+    Pouches: [
+      {
+        image: "https://cdn.pacdora.com/render/20231122/102304880001.jpg",
+        showName:
+          "Flexible packaging stand up zipper pouches mylar bag mockup 600040",
+        modelId: 600040,
+      },
+      {
+        image: "https://cdn.pacdora.com/render/20231122/102305580001.jpg",
+        showName:
+          "Flexible packaging paper bags stand up pouches mockup 600060",
+        modelId: 600060,
+      },
+    ],
+  };
+  const category = new URLSearchParams(
+    new URL(window.location.href).search
+  ).get("name");
+  console.log("category", category);
+  const list = result.code === 422 ? backupData[category] : result.data;
   let html = "";
   for (let i = 0; i < list.length; i++) {
     const item = list[i];
@@ -28,5 +73,13 @@
         </a>
     `;
   }
-  document.querySelector("#list").innerHTML = html;
+  const listDom = document.querySelector("#list");
+  listDom.innerHTML = html;
+  if (result.code === 422) {
+    const tipsDom = document.createElement("div");
+    tipsDom.style = "color: #ccc;";
+    tipsDom.innerText =
+      "Currently displaying sample data only. To access the full data, please grant the necessary permissions.";
+    listDom.insertAdjacentElement("afterend", tipsDom);
+  }
 })();
--- src/static/js/index.js
+++ src/static/js/index.js
@@ -3,8 +3,27 @@
     // Request to obtain Pacdora categories and display them on the page
     const result = await fetch(`/ctree`).then((res) => res.json());
     let html = "";
-    for (let i = 0; i < result.data.length; i++) {
-      const item = result.data[i];
+    const backupData = [
+      {
+        mockupNameKey: "box-mockups",
+        name: "Boxes",
+        homeConfig: {
+          imgSrc:
+            "//cdn.pacdora.com/ui/home/mockups/b1f218f2-257b-4bee-9129-531875dcb52d.webp",
+        },
+      },
+      {
+        mockupNameKey: "pouch-mockups",
+        name: "Pouches",
+        homeConfig: {
+          imgSrc:
+            "//cdn.pacdora.com/ui/home/mockups/39fb8a76-b8af-49ee-928d-5abf79123227.webp",
+        },
+      },
+    ];
+    const list = result.code === 422 ? backupData : result.data;
+    for (let i = 0; i < list.length; i++) {
+      const item = list[i];
       html += `
         <a class="category-item" href="/category.html?key=${item.mockupNameKey}&name=${item.name}">
             <div class="category-name">${item.name}</div>
@@ -19,8 +38,20 @@
     // Request to obtain the list of pre-designed templates and display them on the page
     const templateResult = await fetch("/templates").then((res) => res.json());
     let templateHtml = "";
-    for (let i = 0; i < templateResult.data.length; i++) {
-      const item = templateResult.data[i];
+    const templateBackupData = [
+      {
+        templateId: 20110771,
+        cover: "https://cdn.pacdora.com/preview/cover-156210.jpg",
+      },
+      {
+        templateId: 20110755,
+        cover: "https://cdn.pacdora.com/preview/cover-150020.jpg",
+      },
+    ];
+    const templateList =
+      templateResult.code === 422 ? templateBackupData : templateResult.data;
+    for (let i = 0; i < templateList.length; i++) {
+      const item = templateList[i];
       templateHtml += `
       <a class="list-item" href="/detail.html?templateId=${item.templateId}">
         <img
@@ -35,5 +66,20 @@
     const templateBox = document.getElementById("templates-box");
     templateBox.innerHTML = templateHtml;
     // Pre-designed templates GUI end
+
+    // tipsDom
+    const tipsDom = document.createElement("div");
+    tipsDom.innerText =
+      "Currently displaying sample data only. To access the full data, please grant the necessary permissions.";
+    if (result.code === 422) {
+      const boxTipsDom = tipsDom.cloneNode(true);
+      boxTipsDom.style = "margin-top: 20px; font-size: 14px; color: #ccc;";
+      box.insertAdjacentElement("afterend", boxTipsDom);
+    }
+    if (templateResult.code === 422) {
+      const templateTipsDom = tipsDom.cloneNode(true);
+      templateTipsDom.style = "margin-top: -40px; font-size: 14px; color: #ccc;";
+      templateBox.insertAdjacentElement("afterend", templateTipsDom);
+    }
   } catch (e) {}
 })();
